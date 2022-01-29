2b79cbff7bc39ee00eae646891b4a389a8de0ead
arturfil <arturfil@hotmail.com>
2022-01-29 01:49:10 -0500

    added class and method to display errors

---
+++ src/main/java/com/arturofilio/artus_forms/models/responses/ValidationErrors.java
@@ -0,0 +1,14 @@
+package com.arturofilio.artus_forms.models.responses;
+
+import java.util.Date;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ValidationErrors {
+    private Map<String, String> errors;
+    private Date timestamp;
+}
