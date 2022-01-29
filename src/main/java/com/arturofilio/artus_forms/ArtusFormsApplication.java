38ff6af21a055ad35269ac8f759b4e26f588a5ba
arturfil <arturfil@hotmail.com>
2022-01-29 01:06:31 -0500

    initial backend setup

---
+++ src/main/java/com/arturofilio/artus_forms/ArtusFormsApplication.java
@@ -0,0 +1,14 @@
+package com.arturofilio.artus_forms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtusFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtusFormsApplication.class, args);
+		System.out.println("Server running...");
+	}
+
+}
