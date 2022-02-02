6e60f8827422896e77dd4f07ec56778794724740
arturfil <arturfil@hotmail.com>
2022-02-01 20:53:33 -0500

    added other tests for hash password and display of error in case that there is an attempt to create a user that already exists

--- src/main/java/com/arturofilio/artus_forms/repositories/IUserRepository.java
+++ src/main/java/com/arturofilio/artus_forms/repositories/IUserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUserRepository extends CrudRepository<UserEntity, Long> {
     public UserEntity findByEmail(String email);    
+    public UserEntity findById(long id);
 }
